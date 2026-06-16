0f843eab0acfd3432a0cffacd5a5b033af22b556
buikrisz <buikrisz@gmail.com>
2026-06-16 10:49:14 +0200

    add config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'react-icons'],
   },
 };
 
